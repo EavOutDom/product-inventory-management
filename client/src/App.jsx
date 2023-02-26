671341b11e4fc277a7a6d46e70db9a576e197f48
EavOutDom <eavoutdom400@gmail.com>
2023-02-26 23:08:55 +0700

    fonts

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,8 +6,16 @@ function App() {
     const [count, setCount] = useState(0);
 
     return (
-        <Layout>
-            <div>Hello</div>
+        <Layout style={{ minHeight: "100vh" }}>
+            <h1>Hello h1</h1>
+            <h2>Hello h2</h2>
+            <h3>Hello h3</h3>
+            <h4>Hello h4</h4>
+            <h5>Hello h5</h5>
+            <h6>Hello h6</h6>
+            <div>Hello div</div>
+            <span>Hello span</span>
+            <p>Hello p</p>
             <Outlet />
         </Layout>
     );
