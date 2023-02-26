6fe26b718f877a0590a25a7bb70b03814dfaad3d
EavOutDom <eavoutdom400@gmail.com>
2023-02-26 13:50:06 +0700

    install library and config route

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,32 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import {
+    Route,
+    RouterProvider,
+    createBrowserRouter,
+    createRoutesFromElements,
+} from "react-router-dom";
+import App from "./App";
+import LoginPage from "./routes/login/LoginPage";
+import ErrorPage from "./routes/error/ErrorPage";
+import Product from "./routes/product/Product";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+const router = createBrowserRouter(
+    createRoutesFromElements(
+        <>
+            <Route errorElement={<ErrorPage />}>
+                <Route path="/" index element={<LoginPage />} />
+            </Route>
+            <Route errorElement={<ErrorPage />} element={<App />}>
+                <Route path="/product" index element={<Product />} />
+            </Route>
+        </>
+    )
+);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+    <React.StrictMode>
+        <RouterProvider router={router} />
+    </React.StrictMode>
+);
