fbe3bdd836e057fd908c6ab6b269b13d66f767bb
EavOutDom <eavoutdom400@gmail.com>
2023-03-11 00:29:59 +0700

    change name project

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,117 +7,121 @@ import { BiCategory } from "react-icons/bi";
 import { AppContext } from "./context/appContext";
 
 function getItem(label, key, icon, children) {
-  return {
-    key,
-    icon,
-    children,
-    label,
-  };
+    return {
+        key,
+        icon,
+        children,
+        label,
+    };
 }
 const items = [
-  getItem(
-    <Link to="/product">Product</Link>,
-    "/product",
-    <AiOutlineShoppingCart />
-  ),
-  getItem(<Link to="/category">Category</Link>, "/category", <BiCategory />),
+    getItem(
+        <Link to="/product">Product</Link>,
+        "/product",
+        <AiOutlineShoppingCart />
+    ),
+    getItem(<Link to="/category">Category</Link>, "/category", <BiCategory />),
 ];
 
 function App() {
-  const navigator = useNavigate();
-  const location = useLocation();
-  const {
-    appState: { is_login },
-    appDispatch,
-  } = useContext(AppContext);
-  const [collapsed, setCollapsed] = useState(false);
-  const [titleLayout, setTitleLayout] = useState("");
+    const navigator = useNavigate();
+    const location = useLocation();
+    const {
+        appState: { is_login },
+        appDispatch,
+    } = useContext(AppContext);
+    const [collapsed, setCollapsed] = useState(false);
+    const [titleLayout, setTitleLayout] = useState("");
 
-  useEffect(() => {
-    document.title = titleLayout;
-  }, [titleLayout]);
+    useEffect(() => {
+        document.title = titleLayout;
+    }, [titleLayout]);
 
-  //   useEffect(() => {
-  //     if (is_login === null) return;
-  //     if (!is_login) {
-  //       navigator("/", { replace: true });
-  //       appDispatch({ type: "SET_LOADING", payload: false });
-  //     }
-  //     const handleLocalStorageChange = () => {
-  //       if (is_login) {
-  //         appDispatch({ type: "SET_LOGIN", payload: false });
-  //         navigator("/", { replace: true });
-  //       }
-  //     };
-  //     window.addEventListener("storage", handleLocalStorageChange);
-  //     return () =>
-  //       window.removeEventListener("storage", handleLocalStorageChange);
-  //   }, [is_login]);
+    //   useEffect(() => {
+    //     if (is_login === null) return;
+    //     if (!is_login) {
+    //       navigator("/", { replace: true });
+    //       appDispatch({ type: "SET_LOADING", payload: false });
+    //     }
+    //     const handleLocalStorageChange = () => {
+    //       if (is_login) {
+    //         appDispatch({ type: "SET_LOGIN", payload: false });
+    //         navigator("/", { replace: true });
+    //       }
+    //     };
+    //     window.addEventListener("storage", handleLocalStorageChange);
+    //     return () =>
+    //       window.removeEventListener("storage", handleLocalStorageChange);
+    //   }, [is_login]);
 
-  const handleLogout = () => {
-    navigator("/");
-  };
+    const handleLogout = () => {
+        navigator("/");
+    };
 
-  return (
-    <Layout style={{ height: "100vh", maxHeight: "100vh" }}>
-      <Layout.Sider
-        style={{
-          background: "#f7f9fa",
-          height: "100vh",
-          position: "fixed",
-          left: 0,
-          top: 0,
-          bottom: 0,
-          borderRight: "1px solid #bbb",
-        }}
-        width={300}
-        collapsible
-        collapsed={collapsed}
-        onCollapse={(value) => setCollapsed(value)}
-      >
-        <div className={styles.header}>
-          <img src="/logo.svg" alt="logo" width={40} height={40} />
-          <h1
-            style={{
-              opacity: collapsed ? 0 : 1,
-              transition: "all 0.2s ease",
-            }}
-          >
-            Product management
-          </h1>
-        </div>
-        <Menu
-          mode="inline"
-          items={items}
-          style={{
-            width: "100%",
-            marginTop: 30,
-            padding: 4,
-            background: "#f7f9fa",
-          }}
-          defaultSelectedKeys={[location.pathname]}
-        />
-      </Layout.Sider>
-      <Layout.Content
-        className={styles.layout_content}
-        style={{
-          "--padding-left-width": collapsed ? "80px" : "280px",
-        }}
-      >
-        <div className={styles.layout_content_header}>
-          <h1>{titleLayout}</h1>
-          <div>
-            <Button type="primary" size="large" onClick={handleLogout}>
-              Logout
-            </Button>
-          </div>
-        </div>
-        <div style={{ padding: "0 30px 30px" }}>
-          <Outlet context={[setTitleLayout]} />
-        </div>
-      </Layout.Content>
-    </Layout>
-  );
+    return (
+        <Layout style={{ height: "100vh", maxHeight: "100vh" }}>
+            <Layout.Sider
+                style={{
+                    background: "#f7f9fa",
+                    height: "100vh",
+                    position: "fixed",
+                    left: 0,
+                    top: 0,
+                    bottom: 0,
+                    borderRight: "1px solid #bbb",
+                }}
+                width={300}
+                collapsible
+                collapsed={collapsed}
+                onCollapse={(value) => setCollapsed(value)}
+            >
+                <div className={styles.header}>
+                    <img src="/logo.svg" alt="logo" width={40} height={40} />
+                    <h1
+                        style={{
+                            opacity: collapsed ? 0 : 1,
+                            transition: "all 0.2s ease",
+                        }}
+                    >
+                        Inventory management
+                    </h1>
+                </div>
+                <Menu
+                    mode="inline"
+                    items={items}
+                    style={{
+                        width: "100%",
+                        marginTop: 30,
+                        padding: 4,
+                        background: "#f7f9fa",
+                    }}
+                    defaultSelectedKeys={[location.pathname]}
+                />
+            </Layout.Sider>
+            <Layout.Content
+                className={styles.layout_content}
+                style={{
+                    "--padding-left-width": collapsed ? "80px" : "280px",
+                }}
+            >
+                <div className={styles.layout_content_header}>
+                    <h1>{titleLayout}</h1>
+                    <div>
+                        <Button
+                            type="primary"
+                            size="large"
+                            onClick={handleLogout}
+                        >
+                            Logout
+                        </Button>
+                    </div>
+                </div>
+                <div style={{ padding: "0 30px 30px" }}>
+                    <Outlet context={[setTitleLayout]} />
+                </div>
+            </Layout.Content>
+        </Layout>
+    );
 }
 
 export default App;
--- client/src/routes/login/LoginPage.jsx
+++ client/src/routes/login/LoginPage.jsx
@@ -4,40 +4,52 @@ import { useNavigate } from "react-router-dom";
 import { AppContext } from "../../context/appContext";
 
 const LoginPage = () => {
-  const navigator = useNavigate();
-  const {
-    appState: { is_login },
-  } = useContext(AppContext);
+    const navigator = useNavigate();
+    const {
+        appState: { is_login },
+    } = useContext(AppContext);
 
-  //   useEffect(() => {
-  //     if (is_login === null) return;
-  //     if (is_login) navigator("/role", { replace: true });
-  //   }, [is_login]);
+    //   useEffect(() => {
+    //     if (is_login === null) return;
+    //     if (is_login) navigator("/role", { replace: true });
+    //   }, [is_login]);
 
-  const handleLogin = (value) => {
-    navigator("/product");
-  };
+    const handleLogin = (value) => {
+        navigator("/product");
+    };
 
-  return (
-    <div className="content_center" style={{ height: "100vh", width: "100%" }}>
-      <Card>
-        <h1>Product management</h1>
-        <Form colon={false} labelCol={{ span: 24 }} onFinish={handleLogin}>
-          <Form.Item label={<p>Username</p>} name="username">
-            <Input size="large" />
-          </Form.Item>
-          <Form.Item label={<p>Password</p>} name="password">
-            <Input.Password size="large" />
-          </Form.Item>
-          <Form.Item>
-            <Button htmlType="submit" type="primary" block size="large">
-              Login
-            </Button>
-          </Form.Item>
-        </Form>
-      </Card>
-    </div>
-  );
+    return (
+        <div
+            className="content_center"
+            style={{ height: "100vh", width: "100%" }}
+        >
+            <Card>
+                <h1>Inventory management</h1>
+                <Form
+                    colon={false}
+                    labelCol={{ span: 24 }}
+                    onFinish={handleLogin}
+                >
+                    <Form.Item label={<p>Username</p>} name="username">
+                        <Input size="large" />
+                    </Form.Item>
+                    <Form.Item label={<p>Password</p>} name="password">
+                        <Input.Password size="large" />
+                    </Form.Item>
+                    <Form.Item>
+                        <Button
+                            htmlType="submit"
+                            type="primary"
+                            block
+                            size="large"
+                        >
+                            Login
+                        </Button>
+                    </Form.Item>
+                </Form>
+            </Card>
+        </div>
+    );
 };
 
 export default LoginPage;
